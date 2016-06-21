480f807948bc8e718685f622afc0c6e0c816b19d
odholden <odholden@gmail.com>
2016-06-21 21:31:42 +0100

    installed bourbon

--- src/js/controllers/mainController.js
+++
@@ -1,8 +0,0 @@
-angular
-  .module('AngularGrunt')
-  .controller('MainController', MainController);
-
-function MainController() {
-  var self = this;
-  this.all = ["This is a project to demonstrate the use of Grunt as a helpful task runner."];
-}
